19f0e5a0c65d87902643175caaa1b1d761d0c5d3
bad-ruby hub <nazim.fataliev@list.ru>
2025-02-10 13:57:05 +0400

    +

--- pages/authorization.jsx
+++ pages/authorization.jsx
@@ -1,25 +1,5 @@
-import { useSession, signOut, signIn } from "next-auth/react"
-
 export default function Authorize() {
-    return <>
-       <Account/>
-    </>
-  }
-
-  function Account() {
-    const {data: session} = useSession();
-    console.log("session",session);
-    if (session) {
-      return (
-        <>
-        signed in as {session?.user?.name} ({session?.user?.email})<br/>
-        {session?.user?.image && <img src={session.user.image} style={{width:"50px", borderRadius:"50px"}}/> }
-        <button onClick={()=>signOut()}>выйти</button>
-    </>
-      )
-    }
-    return <>
-    not signet in2 <br/>
-    <button onClick={()=> signIn()}>Войти</button>
-    </>
+    return <h1>
+      Авторизация 
+    </h1>
   }
