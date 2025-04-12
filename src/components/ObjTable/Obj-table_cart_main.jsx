4d7988c76a9283af9ccdf7784d68a3ae2b751264
bad-ruby hub <nazim.fataliev@list.ru>
2025-04-12 19:19:29 +0400

    +

--- src/components/ObjTable/Obj-table_cart_main.jsx
+++ src/components/ObjTable/Obj-table_cart_main.jsx
@@ -104,9 +104,7 @@ export function TableCart({ data, delPost }) {
     if (error) return <>
     <ErrorInfo/>
     </>;
-    if (!productData) return <>
-    <Loader/>
-    </>;
+    if (!productData) return
     const
         countPrice = productData.reduce((acc, obj) => acc + obj.price, 0),
         discount = Math.round(countPrice * 0.25);
